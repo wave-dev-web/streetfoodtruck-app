9dec2e477187ff58626474973e28d37382633267
kimbro97 <rlagudwog@gmail.com>
2025-03-08 02:04:27 +0900

    feat: 로그인 스크린 작업

---
+++ src/utils/validate.ts
@@ -0,0 +1,29 @@
+type UserInfomation = {
+    email: string;
+    password: string;
+}
+
+function validateUser(values: UserInfomation) {
+    const errors = {
+        email: "",
+        password: "",
+    }
+
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(values.email)) {
+        errors.email = "올바른 이메일 형식이 아닙니다."
+    }
+
+    if (!(values.password.length >= 8 && values.password.length <= 20)) {
+        errors.password = '비밀번호는 8~20자 사이로 입력해주세요.';
+    }
+
+    return errors
+}
+
+function validateLogin(values: UserInfomation) {
+    return validateUser(values)
+}
+
+export {
+    validateLogin
+}
